5cf9a95f0a5a044e9a8f9969802afb4b0f8bcf39
pulkitd2699 <pulkitd2699@gmail.com>
2020-06-20 02:02:02 -0700

    mongoose 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -7,26 +7,47 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    // var newDish = Dishes({
+    //     name: 'pizza',
+    //     description: 'test'
+    // });
+
+    //alternate definition
+    Dishes.create({
         name: 'pizza',
         description: 'test'
-    });
+    })
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+    // newDish.save()
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        // return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: {description: 'Updated test'},
+        },{
+            new: true
+        }).exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'nice!!',
+            author: 'Someone'
         });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
